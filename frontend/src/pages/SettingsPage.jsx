2a95c83a971703175963c376e429adc117cbcfa4
vahghar <raghav10g@gmail.com>
2025-04-01 18:29:22 +0530

    build

--- frontend/src/pages/SettingsPage.jsx
+++ frontend/src/pages/SettingsPage.jsx
@@ -51,7 +51,7 @@ const SettingsPage = () => {
                                 <div className="px-4 py-3 border-b border-base-300 bg-base-100">
                                     <div className="flex items-center gap-3">
                                         <div className="w-8 h-8 rounded-full bg-primary flex items-center justify-center text-primary-content font-medium">
-                                            J
+                                            B
                                         </div>
                                         <div>
                                             <h3 className="font-medium text-sm">Bob Khanna</h3>
