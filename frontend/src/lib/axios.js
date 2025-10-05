01dc7e11b5e236785afe4220e97a84dea90868c7
vahghar <raghav10g@gmail.com>
2025-10-06 04:29:37 +0530

    added-backend-url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios"
 
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5001";
+
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: `${BASE_URL}/api`,
     withCredentials: true,
 });
