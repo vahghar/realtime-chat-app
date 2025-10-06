b2d696d6f42f0dd3be4e1bbcdde19c9ab4696ca6
vahghar <raghav10g@gmail.com>
2025-10-06 20:31:52 +0530

    work

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-
+//redeployment
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
